27c37048512faf44d9d83d39e78a90d7cb1addf9
Caleb Horton <c.alex.horton@gmail.com>
2022-11-25 15:42:23 -0600

    added login to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
 import Head from 'next/head'
+import { useSession, signIn } from 'next-auth/react'
 import Button from '../components/Button/Button'
 import Link from 'next/link'
 import Layout from '../components/Layout/Layout'
 import Logo from '../components/Logo/Logo'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 export default function Home() {
+  const { data: session } = useSession()
+  const [redirectStarted, setRedirectStarted] = useState(false)
+  const router = useRouter()
+
+  if (session) {
+    if (!redirectStarted) {
+      router.push('/quiz')
+      setRedirectStarted(true)
+    }
+
+    return (
+      <Layout>
+        <main className='main'>
+          <h1>Redirecting...</h1>
+        </main>
+      </Layout>
+    )
+  }
+
   return (
     <Layout>
       <div className={'container'}>
@@ -16,8 +38,9 @@ export default function Home() {
 
         <main className={'main'}>
           <Logo />
+          <Button content='Sign In' func={() => signIn()} />
           <Link href={'/quiz'}>
-            <Button content='Start Quiz' />
+            <Button content='Continue as Guest' />
           </Link>
         </main>
       </div>
